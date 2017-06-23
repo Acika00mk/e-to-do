17eca8d03133da67e5ffd37b266f0efa781e5973
Aleksandar Kostadinovski <aleks@acika00mk.local>
2017-06-23 16:40:42 +0200

    frame

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ let win
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        frame: false,
+        toolbar: false
     })
+
     win.loadURL(url.format({
 //        pathname: path.join(__dirname, '/ea-ionic/www/index.html'),
         pathname: path.join(__dirname, '/app/index.html'),
-        protocol: 'file:',
-        slashes: true,
-        toolbar: false
+        protocol: 'file:'
     }))
     // Open the DevTools.
     //  win.webContents.openDevTools()
